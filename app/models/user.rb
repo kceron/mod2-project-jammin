d89919c38aa3466c8dd00de2243e2e213f29fd1f
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-28 13:51:36 -0400

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :password, length: { in: 6..10 }
-
-
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ Genre.create([
 
 Song.create([
   {
-    title: "Backstreet;s Back",
+    title: "Backstreet's Back",
     duration: 180,
     artist_id: 3,
     genre_id: 1
@@ -70,65 +70,67 @@ Song.create([
   }
 ])
 
-# User.create!(name: 'foo',
-#   username: ""
-#   password_digest: User.digest('foobar') )
+User.create([
+  {
+    name: "Hugo",
+    username: "hugo4lyfe",
+    password: "hugohugo",
+    password_confirmation: "hugohugo"
+  },
+  {
+    name: "Mary",
+    username: "notamadonna",
+    password: "marymary",
+    password_confirmation: "marymary"
+  }
+  ])
 
-# User.create([
-#   {
-#     name: "Hugo"
-#   },
-#   {
-#     name: "Mary"
-#   }
-#   ])
+Playlist.create([
+  {
+    name: "Fun Tunes",
+    user_id: 1
+  },
+  {
+    name: "Party",
+    user_id: 1
+  },
+  {
+    name: "Sad Times",
+    user_id: 2
+  }
+])
 
-# Playlist.create([
-#   {
-#     name: "Fun Tunes",
-#     user_id: 1
-#   },
-#   {
-#     name: "Party",
-#     user_id: 1
-#   },
-#   {
-#     name: "Sad Times",
-#     user_id: 2
-#   }
-# ])
-
-# SongPlaylist.create([
-#   {
-#     song_id: 1,
-#     playlist_id: 1
-#   },
-#   {
-#     song_id: 2,
-#     playlist_id: 1
-#   },
-#   {
-#     song_id: 3,
-#     playlist_id: 2
-#   },
-#   {
-#     song_id: 4,
-#     playlist_id: 2
-#   },
-#   {
-#     song_id: 5, 
-#     playlist_id: 3
-#   },
-#   {
-#     song_id: 1,
-#     playlist_id: 3
-#   },
-#   {
-#     song_id: 3,
-#     playlist_id: 1
-#   },
-#   {
-#     song_id: 2,
-#     playlist_id: 2
-#   }
-# ])
+SongPlaylist.create([
+  {
+    song_id: 1,
+    playlist_id: 1
+  },
+  {
+    song_id: 2,
+    playlist_id: 1
+  },
+  {
+    song_id: 3,
+    playlist_id: 2
+  },
+  {
+    song_id: 4,
+    playlist_id: 2
+  },
+  {
+    song_id: 5, 
+    playlist_id: 3
+  },
+  {
+    song_id: 1,
+    playlist_id: 3
+  },
+  {
+    song_id: 3,
+    playlist_id: 1
+  },
+  {
+    song_id: 2,
+    playlist_id: 2
+  }
+])
