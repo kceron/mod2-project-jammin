aefa9b510070078bb1fdf19cbcf78b6fd2ac7980
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-28 17:59:25 -0400

    does not include errors on log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :login
     def login
-        session[:user_id]=user_id
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
 end
